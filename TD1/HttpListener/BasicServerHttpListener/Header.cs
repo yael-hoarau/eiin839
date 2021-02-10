1bb67f25098f476a143ebce53668c994d015edf9
Yaël HOARAU <yael.hoarau@etu.univ-cotedazur.fr>
2021-02-10 16:16:23 +0100

    Rendu TP1

---
+++ TD1/HttpListener/BasicServerHttpListener/Header.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Header
+    {
+        HttpListenerRequest request;
+        public Header(HttpListenerRequest r) {
+            request = r;
+        }
+
+        public override string ToString()
+        {
+            string toReturn = "Liste des headers : ";
+            for (int i = 0; i < request.Headers.Count; ++i)
+                toReturn += "<br>" + request.Headers.Keys[i] + " : " + request.Headers[i];
+            return toReturn;
+        }
+    }
+}
