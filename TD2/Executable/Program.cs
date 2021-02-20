b2c47d59f565fee9bea055fa18f189e0c31f2f4e
Yaël HOARAU <yael.hoarau@etu.univ-cotedazur.fr>
2021-02-20 23:15:51 +0100

    TD2 WebDynamic

---
+++ TD2/Executable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Executable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+                Console.WriteLine("< HTML >< BODY > Hello " + args[0] + " et " + args[2] + " </ BODY ></ HTML > ");
+            else
+                Console.WriteLine("Executable <string param1, string param2>");
+        }
+    }
+}
---
+++ TD2/WebDynamic/Mymethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDynamic
+{
+    class Mymethods
+    {
+        public Mymethods()
+        {
+        }
+
+        static public String displayParams(String param1, String param2)
+        {
+            return "<HTML><BODY> Hello " + param1 + " et " + param2 + "</BODY></HTML>";
+        }
+
+        static public String Executable(String param1, String param2)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "C:\\Users\\yaho2\\source\\repos\\eiin839\\TD2\\Executable\\bin\\Debug\\Executable.exe"; // Specify exe name.
+            start.Arguments = $"{param1} {param2}"; // Specify arguments.
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+
+            using (Process process = Process.Start(start))
+            {
+                //
+                // Read in all the text from the process with the StreamReader.
+                //
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    return result;
+                }
+            }
+        }
+    }
+}
